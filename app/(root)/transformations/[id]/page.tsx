ac6e23a9b2953c94cdc5bb73f2b8e4199753a3ff
Boris Nikolic <borisnikolic2302@gmail.com>
2024-02-16 14:56:48 +0100

    clerk auth

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransofrmationsPage = () => {
+  return <div>TransofrmationsPage</div>;
+};
+
+export default TransofrmationsPage;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateTransormationPage = () => {
+  return <div>UpdateTransormationPage</div>;
+};
+
+export default UpdateTransormationPage;
